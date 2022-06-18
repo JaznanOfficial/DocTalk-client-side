aa2ef296f682fedeeecf6e80aac00a62ec56fd7f
JaznanOfficial <jaznanofficial@gmail.com>
2022-06-18 18:06:59 +0600

    some changes

--- src/Components/Pages/NotFoundPage/NotFoundPage.js
+++ src/Components/Pages/NotFoundPage/NotFoundPage.js
@@ -6,17 +6,26 @@ import "./NotFoundPage.css";
 const NotFoundPage = () => {
     return (
         <Box className="not-found-div">
-            <h1 style={{ fontSize: "5rem", marginTop: "0", paddingTop: "3rem", color: "#1F2278" }}>
-                Page Not Found
-            </h1>
-            <Link to='/'><Button variant="outlined" style={{color:'#F1386A', borderColor:'#F1386A'}}>
-            Go Home
-          </Button></Link>
-            <div style={{display:'flex', alignItems:'center', justifyContent:'center'}}>
-                <h1 style={{ fontSize: "10rem", color: "#1F2278" }}>4</h1>
-                <img src="https://meditro.themetrades.com/react/static/media/error.4b5472f1.gif" alt="" width={'100px'} style={{borderRadius:'50%'}} />
-                <h1 style={{ fontSize: "10rem", color: "#1F2278" }}>4</h1>
+            <div style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+                <h1 style={{ fontSize: "7rem", color: "#1F2278" }}>4</h1>
+                <img
+                    src="https://meditro.themetrades.com/react/static/media/error.4b5472f1.gif"
+                    alt=""
+                    width={"100px"}
+                    style={{ borderRadius: "50%" }}
+                />
+                <h1 style={{ fontSize: "7rem", color: "#1F2278" }}>4</h1>
             </div>
+            <h1 style={{ fontSize: "5rem", margin: "0", color: "#1F2278" }}>Page Not Found</h1>
+            <Link to="/">
+                <Button
+                    variant="outlined"
+                    color="error"
+                    style={{ color: "#F1386A", borderColor: "#F1386A", marginTop:'1rem' }}
+                >
+                    Go Home
+                </Button>
+            </Link>
         </Box>
     );
 };
