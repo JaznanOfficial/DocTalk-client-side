3d8fbb7a0f02fd6326b0780045b834af2f580dad
JaznanOfficial <jaznanofficial@gmail.com>
2022-09-30 18:40:41 +0600

    some bug fixed

--- src/Components/Pages/PrivateRoute/PrivateRoute.js
+++ src/Components/Pages/PrivateRoute/PrivateRoute.js
@@ -6,7 +6,9 @@ import useAuth from '../../CustomHooks/useAuth';
 const PrivateRoute = ({ children, ...rest }) => {
     const { isLoading, user } = useAuth();
     const location = useLocation();
-    isLoading && <Skeleton variant="rectangular" width={210} height={118} />;
+    if (isLoading) { 
+        return <Skeleton variant="rectangular" width={210} height={118} />
+    }
     return user.auth? children : <Navigate to='/sign-in' state={{from: location}}></Navigate>
 };
 
